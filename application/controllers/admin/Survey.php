ec403b232c305b5ddd6690a0439500a0a05f7c27
Kwaku <kwaku.nydev@gmail.com>
2022-04-27 22:02:40 +0700

    survey- send email
    Add send survey to  one user function

--- application/controllers/admin/Survey.php
+++ application/controllers/admin/Survey.php
@@ -12,18 +12,18 @@ class Survey extends MY_Controller {
         parent::__construct();
         $this->mHeader['id'] = 'survey';
         $this->mHeader['title'] = 'Survey';
-        $this->mContent['msg'] = ""; 
+        $this->mContent['msg'] = "";
     }
 
     public function list(){
         $this->mHeader['sub_id'] = 'view';
         $this->render("{$this->sub_mLayout}index", $this->mLayout);
     }
-	
+
 	public function get_data(){
 		$this->db->order_by('created_at','desc');
         $table_data['data'] = $this->db->get_where('tbl_survey',array())->result_array();
- 
+
         echo json_encode($table_data);
     }
 
@@ -36,10 +36,10 @@ class Survey extends MY_Controller {
 				'created_at'=>date("Y-m-d H:i:s")
 			);
 			$this->db->insert('tbl_survey',$data);
-			 
+
 			$this->session->set_flashdata('message','Add Question Successfully.');
 		}
-	   
+
         $this->mHeader['sub_id'] = 'add';
         $this->mContent['data'][0]['id']='0';
         $this->render("{$this->sub_mLayout}add", $this->mLayout);
@@ -54,20 +54,20 @@ class Survey extends MY_Controller {
 				'updated_at'=>date("Y-m-d H:i:s")
 			);
 			$this->db->update('tbl_survey',$data,array('id'=>$this->input->post('survey_id')));
-			 
+
 			$this->session->set_flashdata('message','Question updated Successfully.');
 		}
-		
+
         $this->mHeader['sub_id'] = 'view';
         $id = $this->input->get('id');
         $this->mContent['data'] = $this->db->get_where('tbl_survey',array('id'=>$id))->row();
         $this->render("{$this->sub_mLayout}edit", $this->mLayout);
     }
-  
+
 
     public function delete(){
         $id = $this->input->post('id');
-        $this->db->delete('tbl_survey',array('id'=>$id)); 
+        $this->db->delete('tbl_survey',array('id'=>$id));
     }
 
 
@@ -75,60 +75,60 @@ class Survey extends MY_Controller {
         $this->mHeader['sub_id'] = 'view';
         $this->render("{$this->sub_mLayout}result", $this->mLayout);
     }
-	
+
 	public function get_result(){
 		$this->db->select('sr.*,u.name as uname');
 		$this->db->from('tbl_survey_result sr');
 		$this->db->join('tbl_user u','u.id = sr.user_id','left');
 		$this->db->order_by('sr.created_at','desc');
         $table_data['data'] = $this->db->get()->result_array();
-		
+
 		foreach($table_data['data'] as $k=>$v){
 			if(!$table_data['data'][$k]['name']){
 				$table_data['data'][$k]['name'] = $table_data['data'][$k]['uname'];
 			}
 		}
- 
+
         echo json_encode($table_data);
     }
-	
+
 	public function detail(){
 		$this->mHeader['sub_id'] = 'view';
         $id = $this->input->get('id');
 		$this->db->select('sr.*,u.name as uname ,sr.created_at submited');
 		$this->db->join('tbl_user as u','u.id = sr.user_id','left');
         $this->mContent['result'] = $this->db->get_where('tbl_survey_result as sr',array('sr.id'=>$id))->row();
-		
+
 		//echo '<pre>'; print_r($this->mContent['result']);die();
-        
+
 		$this->db->select('s.*,d.detail');
-		$this->db->join('tbl_survey_detail as d','s.id = d.question_id AND result_id="'.$id.'"','left');		
+		$this->db->join('tbl_survey_detail as d','s.id = d.question_id AND result_id="'.$id.'"','left');
 		$this->mContent['survey'] = $this->db->get('tbl_survey as s')->result();
         $this->render("{$this->sub_mLayout}detail", $this->mLayout);
     }
-	
+
 	public function delete_result(){
         $id = $this->input->post('id');
-        $this->db->delete('tbl_survey_detail',array('result_id'=>$id)); 
-        $this->db->delete('tbl_survey_result',array('id'=>$id)); 
+        $this->db->delete('tbl_survey_detail',array('result_id'=>$id));
+        $this->db->delete('tbl_survey_result',array('id'=>$id));
     }
-	
+
 	public function export(){
-		 
+
 		$this->db->select('sr.*,u.name as user_name');
 		$this->db->from('tbl_survey_result sr');
 		$this->db->join('tbl_user u','u.id = sr.user_id','left');
 		$this->db->order_by('sr.created_at','desc');
         $data = $this->db->get()->result_array();
-		
-		foreach($data as $k=>$v){			 
+
+		foreach($data as $k=>$v){
 			$this->db->select('s.*,d.detail');
 			$this->db->join('tbl_survey_detail as d','s.id = d.question_id AND d.result_id="'.$v['id'].'"','left');
 			$detail = $this->db->get('tbl_survey as s')->result_array();
-			 
+
 			$data[$k]['detail'] = $detail;
 		}
-		
+
 		$header = ['No','Name','Created'];
 		foreach($data[0]['detail'] as $k=>$v){
 			$header[] = $v['question'];
@@ -139,24 +139,24 @@ class Survey extends MY_Controller {
 			$row[] = ($k+1);
 			$row[] = ($v['user_name'])?$v['user_name']:$v['name'];
 			$row[] = $v['created_at'];
-			foreach($v['detail'] as $m=>$n){ 
+			foreach($v['detail'] as $m=>$n){
 				$content = json_decode($n['content'],true);
 				$detail = json_decode($n['detail'],true);
 				switch($n['type']){
 					case 1:
 						$row[] = $detail['answer'][0];
 						break;
-					case 2: 
+					case 2:
 						$row[] = implode("; ",$detail['answer']);
 						break;
 					case 3:
 						$tmp = [];
 						foreach($content as $x=>$y){
 							$tmp[] = $y.':'.$detail['answer'][$x];
-						}		
+						}
 						$row[] = implode('; ',$tmp);
 						break;
-					
+
 				}
 
 			}
@@ -168,56 +168,64 @@ class Survey extends MY_Controller {
 		header("Expires: 0");
 
 		$handle = fopen('php://output', 'w');
-		
+
 		fputcsv($handle, $header);
 		foreach ($rows as $data_array) {
 			fputcsv($handle, $data_array);
 		}
 			fclose($handle);
 		exit;
-		
-		 
+
+
 	}
-	
+
 	public function email(){
-		$this->mHeader['sub_id'] = 'view'; 
-        $this->render("{$this->sub_mLayout}email", $this->mLayout); 
+		$this->mHeader['sub_id'] = 'view';
+		$this->mContent['users'] = $this->db->get_where('tbl_user')->result_array();
+        $this->render("{$this->sub_mLayout}email", $this->mLayout);
 	}
-	
+
 	public function sendemail(){
 		 $subject = $this->input->post('subject');
 		 $email_content = $this->input->post('content');
+
+		 $type = $this->input->post('type');
+		 if($type){
+			$user_id = intval($this->input->post('user'));
+			$this->db->where('id',$user_id);
+		 }
+
 		 $data = $this->db->get_where('tbl_user')->result_array();
-		 //echo '<pre>';print_r($data);die();
+		//echo '<pre>';print_r($data);die();
 		 foreach($data as $k=>$v){
-			 $email = $v['email']; 
+			 $email = $v['email'];
 			 $link = site_url('survey/?hash='.md5($email));
 			 $survey = '<br><br>Please using access survey link bellow:<br><a href="'.$link.'">'.$link.'</a>';
 			 $content = 'Hi, '.$v['name']. "<br/>".$email_content.$survey;
-			 
+
 			 if($email){
 				 $this->sendMail($email, $content, $subject);
 			 }
-			 
+
 		 }
 		 echo json_encode(array('status'=>1,'message'=>''.count($data).' emails has sent.'));
-	} 
+	}
 	public function sendMail($toEmail='' , $email_content = '' , $subject = '')
     {
         $mail = new PHPMailer();
-                    
+
         $mail->IsSMTP();
         $mail->Host = 'localhost';
         $mail->SMTPAuth = false;
         $mail->From = 'support@ncdeliteveterans.org';
         $mail->FromName = 'Elite Nor-Cal';
-        
+
         $mail->AddAddress($toEmail);
 
         $mail->IsHTML(true);
         $mail->Subject = $subject;
-        $mail->MsgHTML($email_content); 
-        
+        $mail->MsgHTML($email_content);
+
         return $mail->Send();
     }
 }
--- application/views/admin/survey/email.php
+++ application/views/admin/survey/email.php
@@ -11,7 +11,7 @@
 <div class="page-header page-header-light">
     <div class="page-header-content header-elements-md-inline">
         <div class="page-title d-flex">
-            <h4><span class="font-weight-semibold">Send Survey without Login</span></h4> 
+            <h4><span class="font-weight-semibold">Send Survey without Login</span></h4>
         </div>
     </div>
 </div>
@@ -21,12 +21,29 @@
 <div class="content">
 
     <!-- Basic modals -->
-    <div class="card">        
+    <div class="card">
         <div class="card-body">
-              
+
             <div class="col-md-12">
                  <form class="form-validate-jquery" method="post" id="sendEmail" action="<?php echo site_url('admin/survey/sendemail')?>">
-        		 
+					 <div class="form-group row">
+						 <label class="col-form-label col-lg-2">Type</label>
+						 <div class="col-lg-10">
+							 <select type="text" class="select2-choice form-control" name="type" id="type" required>
+								 <option value="">All Members</option>
+								 <option value="1">Individual</option>
+							 </select>
+							 <div id="individual_wrap" style="margin-top: 30px; display: none;">
+								 <div><input type="text" class="form-control" placeholder="Filter by Name" id="filter"/></div>
+								 <div class="membersList" style="max-height: 300px; margin-top: 10px; overflow-y: auto;">
+									 <?php foreach ($users as $u):
+										 ?>
+									 <div><label><input type="radio" value="<?php echo $u['id']?>" name="user"/> <?php echo $u['name']?> (<?php echo $u['email']?>)</label></div>
+									 <?php endforeach;?>
+								 </div>
+							 </div>
+						 </div>
+					 </div>
 				<div class="form-group row">
 					<label class="col-form-label col-lg-2">Subject *</label>
 					<div class="col-lg-10">
@@ -38,10 +55,10 @@
 					<div class="col-lg-10">
 						<textarea rows="5" cols="3" class="form-control" id="content" name="content" placeholder="Please Enter content" required></textarea>
 					</div>
-				</div> 
+				</div>
 				<div class="form-group row" >
 					<label class="col-form-label col-lg-2"> </label>
-					<div class="col-lg-10">				
+					<div class="col-lg-10">
 					<button type="submit" value="submit" name="submit" class="btn btn-primary" >&nbsp;&nbsp;Submit&nbsp&nbsp; <i id="loadding" style="display:none" class="icon-spinner spinning hide loading"></i></button>
 					</div>
 				</div>
@@ -50,8 +67,8 @@
 					<div class="col-lg-10">
 						<div id="survey_msg"><div>
 					</div>
-				</div> 
-				
+				</div>
+
 			</form>
             </div>
         </div>
@@ -60,13 +77,27 @@
 
 </div>
 <!-- /content area -->
- 
+
 
 <script>
-     
+	$('#type').change(function (){
+		var val = $(this).val();
+		if(val){
+			$('#individual_wrap').show();
+		}else{
+			$('#individual_wrap').hide();
+		}
+	})
+
+	$('#filter').keyup(function(){
+		var sSearch = this.value;
+		$('.membersList > div').hide();
+		$('.membersList > div:contains("' + sSearch + '")').show();
+	});
+
 	var sending = false;
 	$('#sendEmail').submit(function(e){
-		e.preventDefault();		
+		e.preventDefault();
 		if(sending){
 			return false;
 		}
@@ -76,7 +107,7 @@
 		jQuery.ajax({
                 type: "POST",
                 url: "<?php echo base_url(); ?>" + "admin/survey/sendemail",
-                data: $('#sendEmail').serialize() , 
+                data: $('#sendEmail').serialize() ,
                 dataType: 'json',
                 success: function (res) {
 					sending = false;
@@ -90,5 +121,5 @@
 				  }
 		});
 	});
-  
+
 </script>
