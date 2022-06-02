d7593b67573497aa212e4793008c83303648f220
Kwaku <kwaku.nydev@gmail.com>
2022-06-02 12:20:35 +0700

    Update capsta.php

--- application/views/admin/survey/capsta.php
+++ application/views/admin/survey/capsta.php
@@ -56,108 +56,113 @@
 		</div>
 
 		<div style="margin-top: 10px">
-			<table width="100%" style="font-size: 13px;">
-				<tr>
-					<td width="45%" style="vertical-align: top">
-						<div class="box left">
-							<div class="title arrow">CORPORATE PROFILE</div>
-							<div class="content">
-								<?php echo $company_name;?>, is an <?php echo getSelected($survey[3])?> <?php echo getSelected($survey[0])?> owned company  Our company is an entrepreneurial business where we assess client’s needs finding products or services that are beneficial to their everyday life.
-							</div>
-						</div>
-						<div class="box left">
-							<div class="title arrow"><?php echo $company_name;?> has been in business for</div>
-							<div class="content">
-								<?php echo getSelected($survey[2])?> years
-							</div>
-						</div>
-						<div class="box left">
-							<div class="title arrow">Type of company</div>
-							<div class="content">
-								<?php echo getSelected($survey[3])?>
-							</div>
-						</div>
-						<div class="box left">
-							<div class="title arrow">Employees at <?php echo $company_name;?> between</div>
-							<div class="content">
-								<?php echo getSelected($survey[4])?>
-							</div>
-							<div style="margin-top: 5px">
-								<img width="100%" src="<?php echo base_url()?>/assets/capsta/Cap_Sta_15.jpg">
-							</div>
-						</div>
-						<div class="box">
-							<div class="title">CONTACT</div>
-							<div class="content">
-								<p><b>Name:</b> <?php echo $user->name ?><br>
-									<b>Company:</b> <?php echo $user->company ?><br>
-									<?php echo ($user->membership)?'<b>Member:</b> '.$user->membership->name.'<br/>':'<b>Member:</b> non-member'.'<br/>'; ?>
-									<b>Email:</b> <?php echo $user->email ?><br>
-									<b>Phone:</b> <?php echo $user->phone_number ?><br>
-								</p>
-							</div>
-						</div>
+			<table style="width: 100%;">
+				<tr><td valign="middle" style="vertical-align: middle; height: 780px;">
+						<table width="100%" style="font-size: 13px;">
+							<tr>
+								<td width="45%" style="vertical-align: top">
+									<div class="box left">
+										<div class="title arrow">CORPORATE PROFILE</div>
+										<div class="content">
+											<?php echo $company_name;?>, is an <?php echo getSelected($survey[3])?> <?php echo getSelected($survey[0])?> owned company  Our company is an entrepreneurial business where we assess client’s needs finding products or services that are beneficial to their everyday life.
+										</div>
+									</div>
+									<div class="box left">
+										<div class="title arrow"><?php echo $company_name;?> has been in business for</div>
+										<div class="content">
+											<?php echo getSelected($survey[2])?> years
+										</div>
+									</div>
+									<div class="box left">
+										<div class="title arrow">Type of company</div>
+										<div class="content">
+											<?php echo getSelected($survey[3])?>
+										</div>
+									</div>
+									<div class="box left">
+										<div class="title arrow">Employees at <?php echo $company_name;?> between</div>
+										<div class="content">
+											<?php echo getSelected($survey[4])?>
+										</div>
+										<div style="margin-top: 5px">
+											<img width="100%" src="<?php echo base_url()?>/assets/capsta/Cap_Sta_15.jpg">
+										</div>
+									</div>
+									<div class="box">
+										<div class="title">CONTACT</div>
+										<div class="content">
+											<p><b>Name:</b> <?php echo $user->name ?><br>
+												<b>Company:</b> <?php echo $user->company ?><br>
+												<?php echo ($user->membership)?'<b>Member:</b> '.$user->membership->name.'<br/>':'<b>Member:</b> non-member'.'<br/>'; ?>
+												<b>Email:</b> <?php echo $user->email ?><br>
+												<b>Phone:</b> <?php echo $user->phone_number ?><br>
+											</p>
+										</div>
+									</div>
 
-					</td>
-					<td width="30" align="center" valign="middle" style="padding-top: 30px; vertical-align: center;">
-						<img src="<?php echo base_url()?>/assets/capsta/split.jpg">
-					</td>
-					<td width="45%" style="vertical-align: top">
-						<div class="box right">
-							<div class="title arrow">MARKET PROFILE</div>
-							<div class="content">
-								<?php echo $company_name;?> market?
-								<div><?php echo getSelected($survey[5])?></div>
-							</div>
-						</div>
-						<div class="box right">
-							<div class="title arrow">  Contract Bids <?php echo $company_name;?>
+								</td>
+								<td width="30" align="center" valign="middle" style="padding-top: 30px; vertical-align: center;">
+									<img src="<?php echo base_url()?>/assets/capsta/split.jpg">
+								</td>
+								<td width="45%" style="vertical-align: top">
+									<div class="box right">
+										<div class="title arrow">MARKET PROFILE</div>
+										<div class="content">
+											<?php echo $company_name;?> market?
+											<div><?php echo getSelected($survey[5])?></div>
+										</div>
+									</div>
+									<div class="box right">
+										<div class="title arrow">  Contract Bids <?php echo $company_name;?>
 
-							</div>
-							<div class="content">
-								prepared to receive between <?php echo getSelected($survey[7])?>
-							</div>
-						</div>
-						<div class="box right">
-							<div class="title arrow">In what sector do you work? </div>
-							<div class="content">
-								 <?php echo getSelected($survey[9])?>
-							</div>
-						</div>
-						<div class="box">
-							<div class="title">Serving Clients needs <?php echo $company_name;?> could not do it along!</div>
-							<div class="content">
+										</div>
+										<div class="content">
+											prepared to receive between <?php echo getSelected($survey[7])?>
+										</div>
+									</div>
+									<div class="box right">
+										<div class="title arrow">In what sector do you work? </div>
+										<div class="content">
+											<?php echo getSelected($survey[9])?>
+										</div>
+									</div>
+									<div class="box">
+										<div class="title">Serving Clients needs <?php echo $company_name;?> could not do it along!</div>
+										<div class="content">
 
-							</div>
-						</div>
+										</div>
+									</div>
 
-						<div class="box">
-							<div class="content">
-								<div class="border">
-								<table width="100%">
-									<tr>
-										<td width="70%" style="vertical-align: top">
-											<div>Are you a member of supplier diversity?
-												<?php echo getSelected($survey[14])?></div>
-											<div>
-												By become a member of a of supplier diversity group, It will build disable veteran business community and help fellow veteran business owners with resources, training and connecting with corporations and government agencies.
+									<div class="box">
+										<div class="content">
+											<div class="border">
+												<table width="100%">
+													<tr>
+														<td width="70%" style="vertical-align: top">
+															<div>Are you a member of supplier diversity?
+																<?php echo getSelected($survey[14])?></div>
+															<div>
+																By become a member of a of supplier diversity group, It will build disable veteran business community and help fellow veteran business owners with resources, training and connecting with corporations and government agencies.
+															</div>
+
+														</td>
+														<td width="30%">
+															<img width="100%" src="<?php echo base_url()?>/assets/capsta/img3.jpg">
+														</td>
+													</tr>
+												</table>
 											</div>
+										</div>
+									</div>
 
-										</td>
-										<td width="30%">
-											<img width="100%" src="<?php echo base_url()?>/assets/capsta/img3.jpg">
-										</td>
-									</tr>
-								</table>
-							</div>
-							</div>
-						</div>
-
-					</td>
-				</tr>
+								</td>
+							</tr>
+						</table>
+					</td></tr>
 			</table>
+
 		</div>
-		<div style="text-align: center; margin-top: 10px; font-weight: bold; width: 100%; position: fixed; bottom: 10px;">
+		<div style="text-align: center; margin-top: 10px; font-weight: bold; width: 100%; position: fixed; bottom: 0px;">
 			<?php echo $user->name ?> - <?php echo $user->phone_number ?> - <?php echo $user->email ?>
 		</div>
 	</div>
