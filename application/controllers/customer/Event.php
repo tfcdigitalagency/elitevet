a1d18c0f026673c0379548b7c43f5451c7991be8
Kwaku <kwaku.nydev@gmail.com>
2022-06-23 01:55:51 +0700

    Update Event.php

--- application/controllers/customer/Event.php
+++ application/controllers/customer/Event.php
@@ -2,52 +2,52 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Event extends MY_Controller {
-    public $mLayout = 'customer/';
-    public $sub_mLayout = 'customer/event/';
+	public $mLayout = 'customer/';
+	public $sub_mLayout = 'customer/event/';
 
-    function __construct() {
-        parent::__construct();
-        $this->mHeader['id'] = 'event';
-        $this->mHeader['title'] = 'Event';
-        $this->mContent['msg'] = "";
-        $this->load->model(['Event_model','Reg_history_model','User_model','Webinar_model']);
-    }
+	function __construct() {
+		parent::__construct();
+		$this->mHeader['id'] = 'event';
+		$this->mHeader['title'] = 'Event';
+		$this->mContent['msg'] = "";
+		$this->load->model(['Event_model','Reg_history_model','User_model','Webinar_model']);
+	}
 
-    /*
-    * Event
-    * */
-    public function index(){
-        $this->mHeader['sub_id'] = 'event';
-        $this->mContent['event'] = $this->Event_model->get_Event(1);
-        $this->mContent['cur_page']=1;
-        $this->mContent['cur_count']=8;
-        $this->mContent['all_count']=$this->Event_model->count();
-        $this->render("{$this->sub_mLayout}index", $this->mLayout);
-    }
+	/*
+	* Event
+	* */
+	public function index(){
+		$this->mHeader['sub_id'] = 'event';
+		$this->mContent['event'] = $this->Event_model->get_Event(1);
+		$this->mContent['cur_page']=1;
+		$this->mContent['cur_count']=8;
+		$this->mContent['all_count']=$this->Event_model->count();
+		$this->render("{$this->sub_mLayout}index", $this->mLayout);
+	}
 
-    public function display_Event(){
-        $this->mHeader['sub_id'] = 'event';
-        $page = $this->input->get('page');
-        $this->mContent['event'] = $this->Event_model->get_Event($page);
-        $this->mContent['cur_page']=$page;
-        $this->mContent['cur_count']=$page * 8;
-        $this->mContent['all_count']=$this->Event_model->count();//var_dump($this->mContent['cur_count']);die();
-        $this->render("{$this->sub_mLayout}index", $this->mLayout);
-    }
+	public function display_Event(){
+		$this->mHeader['sub_id'] = 'event';
+		$page = $this->input->get('page');
+		$this->mContent['event'] = $this->Event_model->get_Event($page);
+		$this->mContent['cur_page']=$page;
+		$this->mContent['cur_count']=$page * 8;
+		$this->mContent['all_count']=$this->Event_model->count();//var_dump($this->mContent['cur_count']);die();
+		$this->render("{$this->sub_mLayout}index", $this->mLayout);
+	}
 
-    public function display_detail(){
-        $this->mHeader['sub_id'] = 'event';
-        $id = $this->input->get('id');
+	public function display_detail(){
+		$this->mHeader['sub_id'] = 'event';
+		$id = $this->input->get('id');
 
 		$this->session->set_userdata('event_id', $id);
 
-        $this->mContent['data'] = $this->Event_model->find(array("id"=>$id), array(), array(), true);
-        $this->mContent['registed_count'] = $this->Reg_history_model->count(array("event_id"=>$id));
+		$this->mContent['data'] = $this->Event_model->find(array("id"=>$id), array(), array(), true);
+		$this->mContent['registed_count'] = $this->Reg_history_model->count(array("event_id"=>$id));
 
-        $this->render("{$this->sub_mLayout}detail", $this->mLayout);
-    }
+		$this->render("{$this->sub_mLayout}detail", $this->mLayout);
+	}
 
-    public function insert_RegEvent()
+	public function insert_RegEvent()
 	{
 
 		$data = $this->input->post();
@@ -100,39 +100,40 @@ class Event extends MY_Controller {
 	}
 
 	public function google($id =''){
-			if(!$id){
-				$id = $this->session->userdata('event_id');
-			}
-			$webinar = $this->Webinar_model->find(array("id" => $id), array(), array(), true);
-			if(count($webinar) > 0) {
-				$webinar = $webinar[0];
-			}
-			if(isset($_GET['code'])) {
-				try {
-					$this->load->libary('Googlecalendarapi');
+		$this->load->library('Googlecalendarapi');
+		if(!$id){
+			$id = $this->session->userdata('event_id');
+		}
+		$webinar = $this->Webinar_model->find(array("id" => $id), array(), array(), true);
+		if(count($webinar) > 0) {
+			$webinar = $webinar[0];
+		}
 
-					// Get the access token
-					$data = $this->Googlecalendarapi->GetAccessToken(CLIENT_ID, CLIENT_REDIRECT_URL, CLIENT_SECRET, $_GET['code']);
+		if(isset($_GET['code'])) {
+			try {
+				// Get the access token
+				$data = $this->googlecalendarapi->GetAccessToken(CLIENT_ID, CLIENT_REDIRECT_URL, CLIENT_SECRET, $_GET['code']);
 
-					// Save the access token as a session variable
-					$_SESSION['access_token'] = $data['access_token'];
+				// Save the access token as a session variable
+				$_SESSION['access_token'] = $data['access_token'];
 
-					// Redirect to the page where user can create event
-					//header('Location: home.php');
-					$user_timezone = $this->Googlecalendarapi->GetUserCalendarTimezone($_SESSION['access_token']);
+				// Redirect to the page where user can create event
+				$user_timezone = $this->googlecalendarapi->GetUserCalendarTimezone($_SESSION['access_token']);
 
-					// Create event on primary calendar
-					$event_id = $this->Googlecalendarapi->CreateCalendarEvent('primary', $webinar['title'], 0, $webinar['event_time'], $user_timezone, $_SESSION['access_token']);
- 					$this->redirect('customer/event');
-				}
-				catch(Exception $e) {
-					echo $e->getMessage();
-					exit();
-				}
+				// Create event on primary calendar
+				$event_id = $this->googlecalendarapi->CreateCalendarEvent('primary', $webinar['name'], 0, $webinar['start_time'], $user_timezone, $_SESSION['access_token']);
+
+				print_r($webinar);
+				die("OK");
 			}
-			if(!$_SESSION['access_token']) {
-				$login_url = 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/calendar') . '&redirect_uri=' . urlencode(site_url('customer/event/google')) . '&response_type=code&client_id=' . CLIENT_ID . '&access_type=online';
-				redirect($login_url);
+			catch(Exception $e) {
+				echo $e->getMessage();
+				exit();
 			}
-   }
+		}
+		if(!$_SESSION['access_token']) {
+			$login_url = 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/calendar') . '&redirect_uri=' . urlencode(site_url('customer/event/google')) . '&response_type=code&client_id=' . CLIENT_ID . '&access_type=online';
+			redirect($login_url);
+		}
+	}
 }
